61887317309d791d745b0e2bbc7e15deafdf17ac
Action0358 <130025889+Action0358@users.noreply.github.com>
2024-12-14 14:52:13 +0900

    アプリケーションのエントリーポイント

---
+++ server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Action0358/todo-app-lite/server/handlers"
+)
+
+func main() {
+	// Restfulなルーティング設定
+	http.HandleFunc("/todos", handlers.TodosHandlers) // 一覧取得、新規作成
+	http.HandleFunc("/todos/", handlers.TodoHandlers) // 特定のタスクの操作
+
+	// サーバー起動
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Println("Error starting server", err)
+		// エラー発生時に main 関数の処理を終了させる
+		return
+	}
+}
