d00baf7145f351b06a7a07a97613ec0bcb4d5199
Action0358 <dai03588010@gmail.com>
2025-01-05 00:51:43 +0900

    SQLの初期化設定をmainファイルに実装

--- server/main.go
+++ server/main.go
@@ -5,17 +5,27 @@ import (
 	"net/http"
 
 	"github.com/Action0358/todo-app-lite/server/handlers"
+	"github.com/Action0358/todo-app-lite/server/storage"
 )
 
 func main() {
+	// SQLite ストレージの初期化
+	storageInstance, err := storage.NewSQLiteStorage("")
+	if err != nil {
+		log.Printf("Faild to initialize SQLite storage: %v", err)
+	}
+
+	// ハンドラーにストレージを渡す
+	handlers.SetStorage(storageInstance)
+
 	// Restfulなルーティング設定
-	http.HandleFunc("/todos", handlers.TodosHandler)  // 一覧取得、新規作成
-	http.HandleFunc("/todos/", handlers.TodoHandler) // 特定のタスクの操作
+	http.HandleFunc("/todos", handlers.TodosHandlers) // 一覧取得、新規作成
+	http.HandleFunc("/todos/", handlers.TodoHandlers) // 特定のタスクの操作
 
 	// サーバー起動
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Println("Error starting server", err)
+		log.Printf("Error starting server: %v", err)
 		// エラー発生時に main 関数の処理を終了させる
 		return
 	}
